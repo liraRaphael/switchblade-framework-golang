e89d1a58c01722acf635e960226bda8ef3109dbc
liraRaphael <48797169+liraRaphael@users.noreply.github.com>
2024-03-29 23:43:28 -0300

    organização e reestrutura da pasta

---
+++ src/core/env_core.go
@@ -0,0 +1,37 @@
+package core
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/liraRaphael/golang-api-lib/src/environment"
+	"github.com/liraRaphael/golang-api-lib/src/util"
+)
+
+const (
+	envDefault = ".env"
+	envFormat  = "%s.env"
+)
+
+func (s *Server) loadEnvs(paths ...string) {
+	godotenv.Load(paths...)
+}
+
+func (s *Server) loadCoreEnvs() {
+	s.profile = strings.ToLower(environment.GetEnvValueOrDefault("application.profile", ""))
+	s.port = environment.GetEnvValueOrDefault("application.port", "8080")
+
+	s.healthCheckActive = util.StringToBool(environment.GetEnvValueOrDefault("application.healthcheck.active", "true"))
+	s.healthCheckUrl = environment.GetEnvValueOrDefault("application.healthcheck.url", "/health/check")
+}
+
+func (s *Server) InitEnvs() {
+	s.loadEnvs(envDefault)
+
+	if len(strings.TrimSpace(s.profile)) > 0 {
+		s.loadEnvs(envDefault, fmt.Sprintf(envFormat, s.profile))
+	}
+
+	s.loadCoreEnvs()
+}
